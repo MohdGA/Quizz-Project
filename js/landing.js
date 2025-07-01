16305739071ee350e5095885bbe110ef810b3203
mohd <generalassemblyse@gmail.com>
2025-07-01 14:33:01 +0300

    create landing page with category page

---
+++ js/landing.js
@@ -0,0 +1,21 @@
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+const settingBtn = document.querySelector('#settingBtn');
+const settingDiv = document.querySelector("#settingDiv");
+console.log(settingDiv)
+
+
+/*-------------- Functions -------------*/
+
+// function selection(event){
+//   if(event.target.id === "settingBtn"){
+//     settingDiv.innerHTML = "";
+// }
+// }
+/*----------- Event Listeners ----------*/
+// settingBtn.addEventListener('click', selection)
